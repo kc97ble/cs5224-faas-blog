26ec16f5f71ca19b32809b565e43a603cec697f2
Kien Nguyen <kc97ble@gmail.com>
2020-04-10 14:03:42 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import Home from "./components/Home";
+import Article from "./components/Article";
+import Manage from "./components/Manage";
+import Edit from "./components/Edit";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/article/:id" component={Article} />
+        <Route exact path="/manage" component={Manage} />
+        <Route exact path="/edit/:id" component={Edit} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
